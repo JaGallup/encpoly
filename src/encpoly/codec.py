e77aeb04f115cd7eb6f94e29cf1f725a54de346e
Matt Riggott <matt.riggott@gmail.com>
2019-03-18 12:49:12 +0000

    Add functions for encoding and decoding

---
+++ src/encpoly/codec.py
@@ -0,0 +1,71 @@
+from itertools import chain
+from math import floor, ceil
+from operator import sub
+
+
+def polyline_round(number):
+    """
+    >>> polyline_round(0)
+    0
+    >>> polyline_round(1)
+    1
+    >>> polyline_round(-1)
+    -1
+    >>> polyline_round(0.5)
+    1
+    >>> polyline_round(-0.5)
+    -1
+    """
+    if number > 0:
+        return floor(number + 0.5)
+    else:
+        return ceil(number - 0.5)
+
+
+def encode_coord(coord, precision):
+    coord = polyline_round(coord * precision) << 1
+    if coord < 0:
+        coord = ~coord
+    encoded_coord = ""
+    while coord >= 0x20:
+        encoded_coord += chr((0x20 | (coord & 0x1F)) + 63)
+        coord >>= 5
+    encoded_coord += chr(coord + 63)
+    return encoded_coord
+
+
+def encode(locations, precision=5):
+    precision = 10 ** precision
+    encoded_coords = []
+    prev_location = (0, 0)
+    for location in locations:
+        encoded_coords.append(
+            encode_coord(p, precision)
+            for p in map(sub, location, prev_location)
+        )
+        prev_location = location
+    return "".join(chain(*encoded_coords))
+
+
+def decode_coords(polyline):
+    coord = 0
+    shift = 0
+    for char in map(ord, polyline):
+        char -= 63
+        coord |= (char & 0x1F) << shift
+        shift += 5
+        if char < 0x20:
+            coord >>= 1
+            yield ~coord if coord & 1 else coord
+            coord = 0
+            shift = 0
+
+
+def decode(polyline, precision=5):
+    precision = 10 ** precision
+    prev_location = (0, 0)
+    i = decode_coords(polyline)
+    for coord in i:
+        location = [coord + prev_location[0], next(i) + prev_location[1]]
+        yield [p / precision for p in location]
+        prev_location = location
